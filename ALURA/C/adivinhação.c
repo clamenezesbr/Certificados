402417bfd1979e8a80482b01089614692a4e1475
Gabriel Menezes <164266706+clamenezesbr@users.noreply.github.com>
2024-05-27 16:13:17 -0300

    Delete ALURA/C directory

--- ALURA/C/adivinhação.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main(void) {
-  printf("***************************************\n");
-  printf("Bem vindo ao nosso jogo de adivinhação.\n");
-  printf("***************************************\n");
-}
